0aa441dd5058481375ac50e88d28c53a16e5c636
Sourav Roy <roys45545@gamil.com>
2024-03-30 04:01:28 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>TechStar</h1>
+        <p>@all rights reserved</p>
+    </div>
+    <div>
+
+        <h5>Follow Us</h5>
+    </div>
+    <div>
+        <a href='http://in.youtube.com' target={"blank"}>Youtube</a>
+        <a href='https://www.facebook.com' target={"blank"}>facebook</a>
+        <a href='https://www.instagram.com' target={"blank"}>Instagram</a>
+    </div>
+    </footer>
+  
+}
+
+export default Footer;
